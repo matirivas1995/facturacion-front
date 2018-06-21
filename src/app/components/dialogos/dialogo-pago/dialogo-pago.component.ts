708378d5a052ee3cefd28b071c00cfa618401571
Rivas <matias.rt@hotmail.com>
2018-06-21 10:29:12 -0400

    Dialogo de pagos funcionando

--- src/app/components/dialogos/dialogo-pago/dialogo-pago.component.ts
+++ src/app/components/dialogos/dialogo-pago/dialogo-pago.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Pago } from '../../../model';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { EstadoCuentaService } from '../../../services/estadoCuenta.service';
 
 @Component({
   selector: 'app-dialogo-pago',
@@ -7,9 +11,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DialogoPagoComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  pago: Pago;
+  operation: String;
+
+  constructor(
+      private fb: FormBuilder,
+      private dialogRef: MatDialogRef<DialogoPagoComponent>,
+      @Inject(MAT_DIALOG_DATA) data) {
+          this.pago = data.pago || new Pago();
+          this.operation = data.operation;
+      }
 
   ngOnInit() {
+      this.form = this.fb.group({
+          Monto: [this.pago.monto, Validators.required],
+          Fecha: [this.pago.fecha],
+          Porcentaje: [this.pago.porcentaje, Validators.required],
+          Estado: [this.pago.estado, Validators.required]
+      });
   }
+
+  close() {
+      this.dialogRef.close({action: 'close'});
+  }
+
+  save() {
+      this.pago.monto = this.form.value.Monto;
+      this.pago.fecha = this.form.value.Fecha;
+      this.pago.porcentaje = this.form.value.Porcentaje;
+      this.pago.estado = this.form.value.Estado;
+
+      this.dialogRef.close({action: 'save',
+                            cliente: this.pago});
+  }
+
+    delete() {
+        this.dialogRef.close({action: 'delete',
+                              cliente: this.pago});
+    }
+
 
 }
--- src/app/components/pago/pago.component.ts
+++ src/app/components/pago/pago.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Pago } from '../../model';
 import { PagoService } from '../../services/pago.service';
+import { MatDialogConfig, MatDialog, MatSnackBar } from '@angular/material';
+import { DialogoPagoComponent } from '../dialogos/dialogo-pago/dialogo-pago.component';
 
 
 @Component({
@@ -11,9 +13,12 @@ import { PagoService } from '../../services/pago.service';
 export class PagoComponent implements OnInit {
 
   pagos: Pago[];
+  pagoSeleccionado: Pago;
   columnsToDisplay = ['id', 'estadoCuenta', 'monto', 'porcentaje', 'fecha', 'estado'];
 
-  constructor(private pagoService: PagoService) { }
+  constructor(private pagoService: PagoService,
+                public dialog: MatDialog,
+                private snackBar: MatSnackBar) { }
 
   ngOnInit() {
     this.getPagos();
@@ -26,4 +31,61 @@ export class PagoComponent implements OnInit {
         this.pagos = pagos;
       });
   }
+
+    seleccionar(pago: Pago) {
+        this.pagoSeleccionado = pago;
+        console.log(this.pagoSeleccionado);
+
+        const dialogConfig = new MatDialogConfig();
+        dialogConfig.disableClose = true;
+        dialogConfig.autoFocus = false;
+        dialogConfig.width = '400px';
+        dialogConfig.data = {operation: 'modify',
+                            pago: this.pagoSeleccionado
+                            };
+
+        const dialogRef = this.dialog.open(DialogoPagoComponent, dialogConfig);
+
+        dialogRef.afterClosed().subscribe(
+          data => {
+            if (data.action === 'save') {
+              this.pagoService.update(data.pago)
+                .then((response) => {
+                  if (response.ok) {
+                    console.log('Pago ' + data.pago.id + ' modificado');
+                    this.snackBar.open('Pago modificado', '', {duration: 2000});
+                    console.log('Recargando Pagos');
+                    this.getPagos();
+                  } else {
+                    console.log('Pago ' + data.pago.id + ' no pudo ser modificado');
+                    this.snackBar.open('Error al modificar pago', '', {duration: 2000});
+                  }
+                    });
+                } else if (data.action === 'delete') {
+                  this.delete(data.pago);
+                } else {
+                  console.log(data);
+                }
+              }
+            );
+      }
+
+
+      delete(pago: Pago) {
+
+          this.pagoService.delete(pago)
+          .then((response) => {
+            if (response.ok) {
+              console.log('Pago ' + pago.id + ' eliminado');
+              this.snackBar.open('Pago eliminado', '', {duration: 2000});
+              console.log('Recargando pagos');
+              this.getPagos();
+            } else {
+              console.log('Pago ' + pago.id + ' no pudo ser eliminado');
+              this.snackBar.open('Error al eliminar pago', '', {duration: 2000});
+            }
+          });
+      }
+
+
 }
